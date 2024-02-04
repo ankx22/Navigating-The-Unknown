303444c9db98db532c94458d96092eec2c3f8b35
Ankit Talele <ankit.talele76@gmail.com>
2024-02-04 16:26:50 -0500

    Added final submission files, report and media..

--- main.py
+++ main.py
@@ -18,17 +18,23 @@ from utils import flow_viz  # in RAFT folder
 from utils.utils import InputPadder  # in RAFT folder
 
 
-DEVICE = 'cuda'
+DEVICE = 'cuda' # Global variable to set device to cuda. Use cuda torch to get good inference times of 0.5s per flow.
 
 ###### -NN Related functions#######################################
-
-
 def load_image(imfile):
+    """
+    Loads the read frames in appropriate format for the network
+    Args:
+        image file (here the the frames we want to use for optical flow)
+    """
     img = torch.from_numpy(imfile).permute(2, 0, 1).float()
     return img[None].to(DEVICE)
 
-
+#########Stream capture thread#####################################
 def recordWorker():
+    """
+    Thread to capture the images from the drone camera
+    """
     j = 0
     while True:
         frame = drone.get_frame_read().frame
@@ -38,22 +44,36 @@ def recordWorker():
         j = j+1
 
 # -################--Post processing-#######################
-
-
 def contour_area(contour):
+    """
+    Finds the contour area
+    Args:
+        contour
+    Returns:
+        Area
+    """
     return cv2.contourArea(contour)
 
-
 def postprocess(i, current_path, image_path):
-    # Load the image
+    """
+    Identifies the gap in the flow image and returns the center of our gap.
+    Args:
+        i: counter used to name the images being saved
+        current_path: Path for the current file directory
+        image_path: path to the flow image that we want to use
+    Returns:
+        cX, cY: Gap center on the image in pixel coordinates
+    """
+    # Load the flow image
     image = cv2.imread(image_path)
 
     # If the image has an alpha channel, remove it
     if image.shape[-1] == 4:
         image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
+    
+    # Load the real frame image
     real_path = current_path + f"/frames/frame{2*i+1:03d}.png"
     real_image = cv2.imread(real_path)
-    real_image2 = cv2.imread(real_path)
     # Convert image to grayscale
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Step 1: Noise reduction with Gaussian blur
@@ -65,7 +85,7 @@ def postprocess(i, current_path, image_path):
     edges = cv2.Canny(adaptive_thresh, 50, 150)
     # Dilate the edges to close the gaps
     dilated_edges = cv2.dilate(edges, np.ones((3, 3), np.uint8), iterations=1)
-    print("statement 4")
+    # print("statement 4")
     # Apply closing to fill in gaps
     closed_edges = cv2.morphologyEx(
         dilated_edges, cv2.MORPH_CLOSE, np.ones((7, 7), np.uint8))
@@ -84,7 +104,7 @@ def postprocess(i, current_path, image_path):
         hull = cv2.convexHull(contours[i])
         hull_list.append(hull)
 
-    # Sort contours by area and get the largest one
+    # Sort contours by area and get the largest one (Which usually is the gap)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:1]
     hulls = sorted(hull_list, key=cv2.contourArea, reverse=True)[:1]
 
@@ -110,24 +130,25 @@ def postprocess(i, current_path, image_path):
     cv2.imwrite(filepath, image)
     filepath2 = current_path + f"/center_real/frame{i:03d}.png"
     cv2.imwrite(filepath2, real_image)
-    # filepath3 = current_path + f"/contour_real/frame{i:03d}.png"
-    # cv2.imwrite(filepath3, real_image2)
+    
 
     return cX, cY
 ##########################################################################################
 
 
 try:
+    # Get the folder paths and create the necessary folders for saving outputs
     current_path = os.path.abspath(__file__)
-    current_path = current_path.replace("test.py", "")
+    current_path = current_path.replace("main.py", "")
 
-    folders_list = ["center", "flow", "flow_outputs", "frames",
-                    "frames_thread", "center_real", "contour_real"]
+    folders_list = ["center", "flow", "frames",
+                    "frames_thread", "center_real"]
     for folder in folders_list:
         folder_path = os.path.join(current_path, folder)
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
+    # Load the NN model
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', help="restore checkpoint")
     parser.add_argument('--path', help="dataset for evaluation")
@@ -144,6 +165,7 @@ try:
     model.to(DEVICE)
     model.eval()
 
+    # Connect to the drone
     drone = Tello()
     drone.connect()
     # CHECK SENSOR READINGS------------------------------------------------------------------
@@ -158,11 +180,11 @@ try:
     time.sleep(2)
     drone.go_xyz_speed(0, 0, 20, 45)
     time.sleep(2)
-    for j in range(0, 7):  # Ignores black frames
+    for j in range(0, 7):  # Ignores black frames we get initially
         frame1 = drone.get_frame_read().frame
 
-    image_center = np.array([480, 360])
-    tol = 200  # 200, 250
+    image_center = np.array([480, 360]) # (h/2,w/2) is fixed as drone captures images in (960,720) sizes
+    tol = 200  # 200, 250 TUNABLE PARAMETER (Tolerance allowed between the image center and gap center for the drone to safely go through window)
     runs = 0
     framei = 0
     flowi = 0
@@ -172,7 +194,7 @@ try:
     while True:
         frame_no = 0
         center_list = []
-        while frame_no < 1:
+        while frame_no < 1: # Perform visual servo and get the flow
 
             try:
                 # ------Servoing------------------
@@ -242,13 +264,14 @@ try:
         center_list = np.array(center_list)
         average_center = np.mean(center_list, axis=0)
 
-        # -----Visual servoing algo------------------
-        if np.linalg.norm(average_center-image_center) <= tol:
+        # -----Once we get the window center do one of the following------------------
+        if np.linalg.norm(average_center-image_center) <= tol: # If dist b/w image center and gap center within tolerance send the drone through
             drone.go_xyz_speed(400, 0, 0, 90)
             time.sleep(3)
             drone.land()
             print(centers_dict)
             break
+        # Otherwise convert the pixel distance between the centers to real world distance through trail and error to send the drone to that spot.
         conversion_factor = 0.20  # 0.20, 0.18, 0.15
         if image_center[0] - average_center[0] > 0:
             y_command = int(conversion_factor *
